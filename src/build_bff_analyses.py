979675929dd486ca22c197b0febe8b71191646d5
Alexandrina BODRUG <alexandrina.bodrug@univ-nantes.fr>
2024-08-02 16:47:30 +0200

    Run and Analysis classes

--- src/build_bff_analyses.py
+++ src/build_bff_analyses.py
@@ -1,34 +1,26 @@
 # author bodrug-a, chatgpt, github Copilot
 # 2024-08-01
-# This script is coded in object oriented Python
-
+#
 import argparse
 import json
 import build_bff_metadata4beacon as bbm 
 
 # Arguments 
 agp = argparse.ArgumentParser(description='Build BFF analyses file')
-agp.add_argument('-s', '--samples', type=str,
-                help='Path to the file containing samples (one sample per line)',
+agp.add_argument('-p', '--phenotypes', type=str,
+                help='Path to the file containing phenotypes (one individual per line)',
                 required=True)
 agp.add_argument('-o', '--output', type=str,
                 help='Path to the output directory',
                 default='data-deliverable/bff/')
 args = agp.parse_args()
 #
-def load_samples(samples):
-    ''' Load samples from a file
+def write_analysis_bff(slist, output):
+    ''' Write the analyses json in a json file
     '''
-    with open(samples, 'r') as f:
-        samples = [line.strip() for line in f if line.strip()]
-    return samples
-#
-# Main
-if __name__ == "__main__":
-    # Load samples
-    slist = load_samples(args.samples)
     count = 0
     analyses = []
+
     for biosample in slist:
         count += 1
         analysis = bbm.Analysis(bbm.analyses['aligner'], 
@@ -37,11 +29,17 @@ if __name__ == "__main__":
                             bbm.analyses['pipelineRef'], 
                             bbm.analyses['variantCaller'],
                             'bsid-' + biosample, 
-                            'anid-' + bbm.analyses['idbasename'] +'_'+str(count), 
+                            'anid-' + bbm.analyses['idbasename'] +'-'+str(count), 
                             'ivid-' + biosample,
-                            'runId' + bbm.analyses['runbasename']+'_'+str(count)
+                            'runId-' + bbm.analyses['runbasename']+'-'+str(count)
                     )
         analyses.append(analysis.to_dict())
-    with open(args.output+'/analyses.json', 'w') as f:
-        json.dump(analyses, f, indent=4)        
+    with open(output+'/analyses.json', 'w') as f:
+        json.dump(analyses, f, indent=4)
+#
+# Main
+if __name__ == "__main__":
+    # Load samples
+    slist = bbm.load_samples(args.phenotypes)
+    write_analysis_bff(slist, args.output)        
     
--- src/build_bff_metadata4beacon.py
+++ src/build_bff_metadata4beacon.py
@@ -1,17 +1,43 @@
 # varibales for BeaconFriendlyFormat metadata
 # classes for BeaconFriendlyFormat categories
 
+# Analyses and Runs: use biosampleid list
+# Cohorts and Datasets : 
+# Individuals and Biosamples
+# GenomicVariations
+
+import pandas as pd
+
+#
 # Metadata
+#
 analyses = {"aligner" : 'dragen,Version="SW: 07.021.624.3.10.8, HW: 07.021.624"', 
             "analysisDate" : "2024-02-13", 
             "pipelineName" : "dragen-variant-caller-for-AIC", 
             "pipelineRef" : "(Blanchet R et al. 2024)", 
             "variantCaller" : 'dragen,Version="SW: 07.021.624.3.10.8, HW: 07.021.624"',
-            "idbasename" : "AIC_analyisid_",
+            "idbasename" : "AIC_analyisid",
             "runbasename" : "SRR00"
             }
+librarySource = {"id": "GENEPIO:0001966", "label": "genomic Source"}
+runs = {'libraryLayout' : 'PAIRED', 
+        'librarySelection' : 'RANDOM', 
+        'librarySource' : librarySource, 
+        'platform' : 'unknown', 
+        'platformModel' : 'unknown'}
+#
+# Functions 
+#
+def load_samples(samples):
+    ''' Load samples from a file containing phenotypes
+    '''
+    df = pd.read_csv(samples, sep=',')
+    samples = df['N°ADN IRT 1'].tolist()
+
+    return samples
+#
 # Classes
-# Class
+#
 class Analysis:
     #
     def __init__(self, aligner, analysisDate, pipelineName, pipelineRef, variantCaller,
@@ -27,7 +53,7 @@ class Analysis:
         self.variantCaller = variantCaller
     #
     def __str_(self):
-        return (f"Analysis(aligner={self.aligner},  \
+        return (f"Analysis(aligner={self.aligner}, \
                 analysisDate={self.analysisDate}, \
                 biosampleId={self.biosampleId}, \
                 id={self.id}, \
@@ -48,4 +74,47 @@ class Analysis:
             'pipelineRef': self.pipelineRef,
             'runId': self.runId,
             'variantCaller': self.variantCaller
+        }
+#
+class Run:
+    #
+    def __init__(self, libraryLayout, librarySelection, librarySource, LibraryStrategy,
+                 platform, platformModel,
+                 biosampleId, id, individualId, runDate):
+        self.libraryLayout = libraryLayout
+        self.librarySelection = librarySelection
+        self.librarySource = librarySource
+        self.LibraryStrategy = LibraryStrategy
+        self.platform = platform
+        self.platformModel = platformModel
+        self.biosampleId = biosampleId
+        self.id = id
+        self.individualId = individualId
+        self.runDate = runDate
+        
+    #
+    def __str_(self):
+        return (f"Run(libraryLayout={self.libraryLayout},  \
+            librarySelection={self.librarySelection}, \
+            librarySource={self.librarySource}, \
+            libraryStrategy={self.LibraryStrategy}, \
+            platform={self.platform}, \
+            platformModel={self.platformModel}, \
+            biosampleId={self.biosampleId}, \
+            id={self.id}, \
+            individualId={self.individualId}, \
+            runDate={self.runDate})")
+    #
+    def to_dict(self):
+        return {
+            'libraryLayout': self.libraryLayout,
+            'librarySelection': self.librarySelection,
+            'librarySource': self.librarySource,
+            'libraryStrategy': self.LibraryStrategy,
+            'platform': self.platform,
+            'platformModel': self.platformModel,
+            'biosampleId': self.biosampleId,
+            'id': self.id,
+            'individualId': self.individualId,
+            'runDate': self.runDate
         }
