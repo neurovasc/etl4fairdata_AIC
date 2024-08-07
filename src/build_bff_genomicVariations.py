86f9c8bec175c212c55629f8a75e5fd672020613
Alexandrina BODRUG <alexandrina.bodrug@univ-nantes.fr>
2024-08-07 11:43:21 +0200

    added genomicVariations frame

---
+++ src/build_bff_genomicVariations.py
@@ -0,0 +1,33 @@
+# # author bodrug-a, chatgpt, github Copilot
+# 2024-08-06
+#
+import argparse
+import json
+import pandas as pd
+import build_bff_metadata4beacon as bbm
+
+# Arguments 
+agp = argparse.ArgumentParser(description='Build BFF genomicVariantions file')
+agp.add_argument('-g', '--genotypes', type=str,
+                help='Path to the file containing genotypes (VCF)',
+                required=True)
+agp.add_argument('-o', '--output', type=str,
+                help='Path to the output directory',
+                default='data-deliverable/bff/')
+args = agp.parse_args()
+#
+# TO DO
+#
+def write_genomicVariations_bff(vcf, output):
+    # TO DO 
+    #
+    genomicVariations = []
+    with open(output+'/genomicVariations.json', 'w') as f:
+        json.dump(genomicVariations, f, indent=4)
+    return 0    
+#
+# Main
+if __name__ == "__main__":
+    # Write bff
+    write_genomicVariations_bff(args.genotypes, args.output) 
+
